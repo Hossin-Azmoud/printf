6ba616315bd2415a08245270e304b3b7f811c75c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-07-21 17:34:17 +0100

    putunbr

---
+++ _putunbr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _putunbr - print the positive number
+* @n: check the arg
+* Return: number bytes printed
+*/
+int _putunbr(unsigned int n)
+{
+    return _putnbr((int) n);
+}
