265aa4e00226f4aea45284adbc147e5822f740ae
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-07-21 19:09:52 +0100

    for merge

--- _printf.c
+++ _printf.c
@@ -6,8 +6,10 @@
  * Returns: the number of characters printed 
  * (excluding the null byte used to end output to strings)
  */
+ /*
 int _printf(const char *format, ...)
 {
 	(void*) format;
 	return (0);
 }
+*/
--- _putunbr.c
+++ _putunbr.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.h"
 /**
 * _putunbr - print the positive number
@@ -8,3 +9,15 @@ int _putunbr(unsigned int n)
 {
     return _putnbr((int) n);
 }
+=======
+#include "main.h"
+/**
+* _putunbr - print the positive number
+* @n: check the arg
+* Return: number bytes printed
+*/
+int _putunbr(unsigned int n)
+{
+	return _putnbr((int) n);
+}
+>>>>>>> B
